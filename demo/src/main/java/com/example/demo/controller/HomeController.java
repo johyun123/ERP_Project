3b55bf724f4a59ecade3a8feac3d5f21c5a2b4c9
yeunwoo3620KR123 <yeunwoo3620@gmail.com>
2026-03-11 17:24:12 +0900

    로그인 회원가입 완성
    로그인 아이디 비밀번호 둘다 222
    
    회원가입 하려면 일단은 /register로 직접 쳐서 들어가야되요

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,32 +2,30 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.Service.LoginService;
-
-import ch.qos.logback.core.model.Model;
+import com.example.demo.Service.UserService;
 
 @Controller
 public class HomeController {
 
-//    // 메인 로그인 페이지
-//    @GetMapping("/")
-//    public String home() {
-//        return "MainPage";
-//    }
-//    
-//    // 메인 페이지 테스트
-//    @GetMapping("/test")
-//    public String system() {
-//        return "NewFile";
-//    }
+//	// 메인 로그인 페이지
+//	@GetMapping("/")
+//	public String home() {
+//		return "MainPage";
+//	}
+//
+//	// 메인 페이지 테스트
+//	@GetMapping("/test")
+//	public String system() {
+//		return "NewFile";
+//	}
 
 	@Autowired
-	private LoginService loginService;
+	private UserService userService; // 타입도 LoginService -> UserService
 
 	@GetMapping("/")
 	public String loginForm() {
@@ -35,14 +33,15 @@ public class HomeController {
 	}
 
 	@PostMapping("/login")
-	public String login(@RequestParam String username, @RequestParam String password, Model model) {
+	public String login(@RequestParam String user_id, @RequestParam String user_pw, Model model) {
 
-		if (loginService.login(username, password)) {
-			model.addAttribute("username", username);
+		if (userService.login(user_id, user_pw)) { // userService 사용
+			model.addAttribute("user_id", user_id);
 			return "home"; // home.jsp
 		} else {
 			model.addAttribute("error", "아이디 또는 비밀번호가 틀렸습니다.");
 			return "login";
 		}
 	}
+
 }
