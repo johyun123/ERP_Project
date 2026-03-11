7c9f43c234ab8f8387c45c50dc6f1ff6d5ee624d
sej040322-droid <sej040322@gmail.com>
2026-03-11 17:39:04 +0900

    "메인구현"

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -18,4 +18,16 @@ public class HomeController {
     public String system() {
         return "NewFile";
     }
+    
+    // 제품관리 페이지
+    @GetMapping("/product")
+    public String product(){
+        return "product";
+    }
+    
+    // 제품등록 페이지
+    @GetMapping("/productRegister")
+    public String productRegister(){
+        return "productRegister";
+    }
 }
