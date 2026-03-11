f01e96fcfd32d55182e73567b0617792c5aa7545
johyun123 <zt6npmmgtg@privaterelay.appleid.com>
2026-03-11 12:11:35 +0900

    "STS 작업틀"

---
+++ demo/src/main/java/com/example/demo/Domain/Test.java
@@ -0,0 +1,6 @@
+package com.example.demo.Domain;
+
+public class Test {
+	int test;
+	String tests;
+}
---
+++ demo/src/main/java/com/example/demo/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class TestMapper {
+	void a() {
+		
+	}
+}
