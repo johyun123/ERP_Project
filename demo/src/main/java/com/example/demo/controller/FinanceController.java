09054c30709b5d980e6fc974180adecdaf56a7fe
johyun123 <zt6npmmgtg@privaterelay.appleid.com>
2026-03-13 11:38:58 +0900

    헤더 완벽 구현!

--- demo/src/main/java/com/example/demo/controller/FinanceController.java
+++ demo/src/main/java/com/example/demo/controller/FinanceController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FinanceController {
 	
+	@GetMapping("/f_register")
+	public String finance(){
+		return "Finance/F_register";
+	}
 }
